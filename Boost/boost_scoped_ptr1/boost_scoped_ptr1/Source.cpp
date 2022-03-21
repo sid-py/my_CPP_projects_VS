44a58dc9e9b7ada5a4611827ab5b81361967a834
siddheshsule <siddhesh.sule47@gmail.com>
2022-03-21 22:54:04 +0100

    File updated

--- Boost/boost_scoped_ptr1/boost_scoped_ptr1/Source.cpp
+++ Boost/boost_scoped_ptr1/boost_scoped_ptr1/Source.cpp
@@ -1,13 +1,26 @@
-#include <boost/scoped_ptr.hpp>
+#include<boost/scoped_array.hpp>
 #include<iostream>
 
-int main() {
-	boost::scoped_ptr<int> p{ new int(1) };
-	std::cout << *p << std::endl;
-
-	p.reset(new(int{ 2 }));
-	std::cout << *p.get() <<std::endl;
+/*
+int main()
+{
+	boost::scoped_ptr<int> p{ new int{1} };
+	std::cout << *p << '\n';
+	p.reset(new int{ 2 });
+	std::cout << *p.get() << '\n';
 	p.reset();
 	std::cout << std::boolalpha << static_cast<bool>(p) << '\n';
-
 }
+*/
+
+
+/*
+int main() {
+	boost::scoped_array<int> p{ new int[2] };
+	*p.get() = 1;
+	p.reset(new int[3]);
+	std::cout << *p.get() << std::endl;
+}
+
+*/
+
---
+++ Boost/boost_scoped_ptr1/boost_scoped_ptr1/ex2.cpp
@@ -0,0 +1,7 @@
+#include<boost/scoped_array.hpp>
+
+int main() {
+	boost::scoped_array<int> p{ new int[2] };
+	*p.get() = 1;
+	p.reset(new int[3]);
+}
